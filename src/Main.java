c56d4b4abe5622fb1044d64f0e9b95e73a27130d
Abdi0947 <debelaabdi3@gmail.com>
2023-11-29 10:03:27 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -99,7 +99,7 @@ class StudentRegistrationSystem {
 
     public void addStudent(Student student) {
         students.add(student);
-        System.out.println("You are registered successfully.");
+        System.out.println("✨✨✨ Hooray! 🎉🎉 Your registration is complete, and you've unlocked a world of endless enchantment and remarkable adventures! ✨✨✨");
         System.out.println("NAME: " + student.getFirstName().toUpperCase() + "     " + student.getLastName().toUpperCase());
         System.out.println("___________________Here is your slip---------------------------------");
         System.out.println("|Course--------------------------------------|ECTS|-----------|cr.hr.|-------Lectures");
@@ -138,78 +138,116 @@ class StudentRegistrationSystem {
                 System.out.println("|Semester:........................." + student.getSemester());
                 System.out.println("|Year:............................." + student.getYear());
                 System.out.println("|\t\t\t\t\t\t\t\t\t|");
-
                 return;
             }
         }
         System.out.println("Sorry, student not found with ID: " + studentId);
     }
+
+    private String getStudentType(String department) {
+        if (department.equalsIgnoreCase("distance")) {
+            return "Distance Education";
+        } else {
+            return "Regular";
+        }
+    }
 }
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         StudentRegistrationSystem registrationSystem = new StudentRegistrationSystem();
-        Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("\nSTUDENT REGISTRATION SYSTEM");
-            System.out.println("1. Add Student");
-            System.out.println("2. Remove Student");
-            System.out.println("3. Display Student Information");
-            System.out.println("4. Exit");
-            System.out.print("Enter your choice: ");
-            int choice = scanner.nextInt();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("╔══════════════════════════════════════════════╗");
+            System.out.println("║                                              ║");
+            System.out.println("║         Welcome to the Student Registration  ║");
+            System.out.println("║                  System                      ║");
+            System.out.println("║          prepared by Abdi Debela             ║");
+            System.out.println("╚══════════════════════════════════════════════╝");
+            System.out.println("------------------------------------------------");
+            System.out.println();
 
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter student first name: ");
-                    String firstName = scanner.next();
+            while (true) {
+                System.out.println("\nPlease select an option:");
+                System.out.println("1. Add a new student");
+                System.out.println("2. Remove a student");
+                System.out.println("3. Display student information");
+                System.out.println("4. Exit");
 
-                    System.out.print("Enter student last name: ");
-                    String lastName = scanner.next();
+                System.out.print("Enter your choice: ");
+                try {
+                    int choice = scanner.nextInt();
+                    scanner.nextLine(); // Consume the newline character
 
-                    System.out.print("Enter student ID: ");
-                    String id = scanner.next();
+                    switch (choice) {
+                        case 1:
+                            System.out.print("Enter student first name: ");
+                            String firstName = scanner.nextLine();
 
-                    System.out.print("Enter student address: ");
-                    String address = scanner.next();
+                            System.out.print("Enter student last name: ");
+                            String lastName = scanner.nextLine();
 
-                    System.out.print("Enter student department: ");
-                    String department = scanner.next();
+                            String id;
+                            while (true) {
+                                System.out.print("Enter student ID (e.g., 0000/00): ");
+                                id = scanner.nextLine();
 
-                    System.out.print("Enter student college: ");
-                    String college = scanner.next();
+                                if (id.matches("\\d+/\\d+")) {
+                                    break;
+                                } else {
+                                    System.out.println("Invalid ID format. Please enter the ID in the correct format (e.g., number/year).");
+                                }
+                            }
 
-                    System.out.print("Enter student semester: ");
-                    int semester = scanner.nextInt();
+                            System.out.print("Enter student college: ");
+                            String college = scanner.nextLine();
 
-                    System.out.print("Enter student year: ");
-                    int year = scanner.nextInt();
+                            System.out.print("Enter student department (Regular/Distance): ");
+                            String department = scanner.nextLine();
 
-                    Student student = new Student(firstName, lastName, id, address, department, college, semester, year);
-                    registrationSystem.addStudent(student);
-                    break;
+                            System.out.print("Enter student address: ");
+                            String address = scanner.nextLine();
 
-                case 2:
-                    System.out.print("Enter student ID to remove: ");
-                    String removeId = scanner.next();
-                    registrationSystem.removeStudent(removeId);
-                    break;
+                            // Prompt for semester and year
+                            System.out.print("Enter student semester: ");
+                            int semester = scanner.nextInt();
 
-                case 3:
-                    System.out.print("Enter student ID to display information: ");
-                    String displayId = scanner.next();
-                    registrationSystem.displayStudentInformation(displayId);
-                    break;
+                            System.out.print("Enter student year: ");
+                            int year = scanner.nextInt();
+                            scanner.nextLine(); // Consume the newline character
 
-                case 4:
-                    System.out.println("Exiting program... Goodbye!");
-                    System.exit(0);
-                    break;
+                            Student student = new Student(firstName, lastName, id, address, department, college, semester, year);
+                            registrationSystem.addStudent(student);
+                            break;
 
-                default:
-                    System.out.println("Invalid choice. Please enter a valid option.");
+                        case 2:
+                            System.out.print("Enter student ID to remove: ");
+                            String removeId = scanner.nextLine();
+
+                            registrationSystem.removeStudent(removeId);
+                            break;
+
+                        case 3:
+                            System.out.print("Enter student ID to display information: ");
+                            String displayId = scanner.nextLine();
+
+                            registrationSystem.displayStudentInformation(displayId);
+                            break;
+
+                        case 4:
+                            System.out.println("Exiting the program...");
+                            System.exit(0);
+
+                        default:
+                            System.out.println("Invalid choice. Please try again.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input. Please enter a number.");
+                }
             }
+
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
         }
     }
 }
